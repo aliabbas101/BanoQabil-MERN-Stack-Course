66ffd73d4a8d515fbdd4a2b90860243eb9e6a3af
aliabbas101 <aliabbas7861@outlook.com>
2023-03-29 18:53:03 +0500

    Week 5 and Week 6 class Examples

---
+++ Week 6 - Lecture 1/script.js
@@ -0,0 +1,179 @@
+let a = 1;
+let b = 2;
+let operation = "sub";
+
+if(operation == "add") {
+    console.log(a + b);
+}
+else if(operation == "sub") {
+    console.log(a-b);
+}
+else if(operation == "multiply") {
+    console.log(a*b);
+}
+else if(operation == "divide") {
+    console.log(a/b);
+}
+else {
+    console.log("Operation not supported");
+}
+
+
+
+
+// let question = "What is javascript ?";
+// let correctAnswer = "programming language";
+
+// let actualAnswer = "programming language";
+
+
+// if (actualAnswer == correctAnswer) {
+//     console.log("First answer is correct");
+// } 
+// else  {
+//     console.log("First answer is not correct");
+// }
+
+/**
+ * == Equal to  
+ * != Not Equal to
+ * >= Greater or equal to
+ * <= lesser than or equal to
+ * < lesser than
+ * > greater than
+ */
+
+// console.log(answer1, answer2);
+
+
+
+// /** Strings */
+// let message = "Your name is: \" double quotes \" ";
+// let myName = "Ali";
+
+// /** Concatenation */
+// let wholeSentence = message + myName;
+// console.log(wholeSentence);
+// console.log(wholeSentence.length);
+
+
+
+
+
+// let a = 1.1;
+// let b = 0.2892832983;
+// let c = -8138918;
+// let d = 9829812;
+
+// let result =  ((a+b) * c) - d;
+// console.log(result);
+/** 5 */
+/** 2 */
+
+/**
+ * BODMAS
+ * Bracket
+ * Open
+ * Division
+ * Multiplication
+ * Addition
+ * Subtraction
+ */
+
+
+
+
+
+
+
+
+// let table = 4;
+
+// console.log(table, " x 1 =", table * 1);
+// console.log(table, " x 2 =", table * 2);
+// console.log(table, " x 3 =", table * 3);
+// console.log(table, " x 4 =", table * 4);
+// console.log(table, " x 5 =", table * 5);
+
+
+/**
+ * 
+ * 2 x 2 = 4
+ * 2 x 3 = 6
+ * .
+ * .
+ * .
+ * 2 x 11 = 22
+ */
+
+
+// let a = 1;
+// let b = 2;
+// let c = a + b;
+// console.log("Your addition result is: ", c);
+
+// let subtraction = a - b;
+
+// console.log("Your subtraction result is: ", subtraction);
+
+
+// let multiplication = a * b;
+// console.log("Your multiplication result is: ", multiplication);
+
+
+// let division = a / b;
+// console.log("Your division result is: ", division);
+
+
+/**
+ * ---- Variable Rules: -----
+ * Unique
+ * It shouls not be a reserved keyword
+ * Case-sensitive
+ */
+
+
+/**
+ * ---- Reserved Keywords -----
+ * let
+ * const
+ * var
+ * for
+ * while
+ * if
+ * else
+ * switch
+ * 
+ */
+
+
+/** 
+ * let, var, const
+ */
+
+/**
+ * Integer | Numeric Digit
+ * Float | Decimal Value
+ * character | ABCabb@@#@#!
+ * string | hello 
+ */
+
+
+
+
+// let a = 1;
+// let b = 2;
+
+// let c = 0;
+
+// console.log("variables before swaping");
+// console.log(a);
+// console.log(b);
+
+// c=a;
+// a=b;
+// b=c;
+
+// console.log("variables after swaping");
+// console.log(a);
+// console.log(b);
