7174c293565b38b931de71b1bb5bd0bfd3991fbe
aliabbas101 <alia63581@gmail.com>
2023-04-25 14:08:22 +0500

    Week 6 - Lecture 3

--- Week 6 - Lecture 3/4_objects.js
+++ Week 6 - Lecture 3/4_objects.js
@@ -1,12 +1,237 @@
+/** Primitive vs Non-primitive data types in JavaScript */
+/***
+ * Number
+ * Boolean
+ * String
+ * Null
+ * Undefined
+ */
+
+/** Almost everything in Javascript is an object */
+
 /** Declaring and Initializing Objects
  * Objects are a collection of key value pairs
  * 
  */
 
+
+/** Car
+ * Properties
+ * Color
+ * wheels
+ * blinds
+ * speed
+ * seats
+ * canPressBreak
+ * cc
+ * 
+ * 
+ * Functionalities
+ * break
+ * accelerator
+ * openAirBags
+ * turnLeft
+ * turnRight
+ * Indicator
+ * 
+ */
+
+var name = "ali";
+
+// var car = {
+//    name: "Audi",
+//     color : "red",
+//     wheels: 4,
+//     canPressBreaks: true,
+//     speed: 120,
+//     seats: 4,
+//     cc: "2000cc",
+//     /** Anonymous FUnction */
+//     accelerate: function() {
+//         console.log("Car is now moving at speed of", this.speed);
+//     },
+//     increaseSpeed: function(speed) {
+//         if(speed > 1){
+//             this.speed = this.speed + speed;
+//         }
+//         else {
+//             console.log("Speed cannot be negative");
+//         }
+//     },
+//     turn: function(direction) {
+//         console.log("Car is now turning, ", direction);
+//     }
+// };
+
+// /** By Reference Assignment */
+// let mehran = car;
+// let cultus = car;
+
+// mehran.speed = 10;
+// mehran.name = "mehran";
+// cultus.name = "cultus";
+
+// console.log(car);
+
+// Encapsulation (Data Hiding)
+
+
+
+// car.wheels = 10;
+// car.accelerate();
+// car.increaseSpeed(10);
+// car.turn("right");
+
+// console.log(car);
+
+
+// console.log(car.name, car.wheels);
+// Constructor Function
+
+function Car(name, color, wheels, seats, speed, cc, canPressBreaks) {
+    
+    /** Public Properties */
+    this.name = name;
+    this.color = color;
+    this.wheels = wheels;
+    this.seats = seats;
+    this.speed = speed;
+    this.cc = cc;
+    this.canPressBreaks = canPressBreaks;
+    
+    /** Private property */
+    let secretProperty = "secret";
+
+    this.accelerate = function () {
+                console.log("Car is now moving at speed of", this.speed);
+                console.log(secretProperty);
+    }
+
+
+            // increaseSpeed: function(speed) {
+            //     if(speed > 1){
+            //         this.speed = this.speed + speed;
+            //     }
+            //     else {
+            //         console.log("Speed cannot be negative");
+            //     }
+            // },
+            // turn: function(direction) {
+            //     console.log("Car is now turning, ", direction);
+            // }
+}
+
+let mehran = new Car("mehran", "red", 4, 4, 120, 2000, true);
+let cultus = new Car("cultus", "silver", 4, 4, 200, 2000, true);
+
+mehran.color = "green";
+console.log(mehran.secretProperty);
+mehran.accelerate();
+console.log(mehran);
+console.log(cultus);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// let car = {
+//     color : "red",
+//     numberOfSeats: 4,
+//     haveRoof: true,
+//     speed: "120 km/h",
+//     breakMissing: false,
+//     drive : function() {
+//         console.log("driving started");
+//         console.log("speed is ", this.speed);
+//     },
+//     break: function() {
+//         if(this.breakMissing) {
+//             console.log("break nahi lag sakta");
+//         }
+//         else {
+//             console.log("lag gaya")
+//         }
+
+//     }
+// };
+
+
+
+// car.numberOfSeats = 7;
+
+/** BY reference assignmen */
+// let mehran = car;
+
+// mehran.color = "silver"; 
+// car.color = "green";
+// car.drive();
+// car.break();
+// console.log(car["color"]);
+
+
 /** Accessing and modifying Object Properties */
+/** Object Mutation (By Reference) */
+
 
 /** Understanding Object Oriented Programming 
  * Objects are a way to model real world things using code
  * Objects are a way to group related variables and functions together
  * 
+ * 
 */
+
+
+/** Object Constructor */
+// function Car(seats, color, wheels, haveRoof, speed, breakMissing) {
+//     this.color = color;
+//     this.seats = seats;
+//     this.wheels = wheels;
+//     this.haveRoof = haveRoof;
+//     this.speed = speed;
+//     this.breakMissing = breakMissing;
+
+//     this.increaseSpeed = function () {
+//         this.speed = "200";
+//     }
+// }
+
+
+// let mehran = new Car(7, "red", "4", true, "120", false);
+// mehran.increaseSpeed();
+// console.log(mehran.speed);
+
+
+
+// let cultus = new Car(4, "silver", "4", true, "120", false);
+// console.log(mehran);
+// console.log(cultus);
+
+/** this Keyword */
+
+
+/** In an object method, this refers to the object. */
+/** Alone, this refers to the global object. */
+/** In a function, this refers to the global object. */
+/** In a function, in strict mode, this is undefined. */
+/** In an event, this refers to the element that received the event. */
+/** Methods like call(), apply(), and bind() can refer this to any object. */
+
+
+
+
+
--- Week 6 - Lecture 3/6_functions.js
+++ Week 6 - Lecture 3/6_functions.js
@@ -1,6 +1,191 @@
-/** Functions in Javascript */
+// /**
+//  * Functions 
+//  * Input
+//  * Process / Complex Mathematical Calculation / Logic
+//  * Output
+//  */
+
+
+
+
+
+
+/** 
+ * add(paramater 1, parameter 2)
+ */
+// function add(a, b) {
+//     let sum = a + b;
+//     return sum;
+// }
+
+// function sub(a, b) {
+//     let sub = a - b;
+//     return sub;
+// }
+
+// function multiply(a, b) {
+//     let mul = a *b;
+//     return mul;
+// }
+
+// function calculator(a,b, operation) {
+//     if (operation == "add") {
+//         return a + b;
+//     }
+//     else if(operation == "sub") {
+//         return a - b;
+//     }
+//     else if(operation == "mul") {
+//         return a * b;
+//     }
+//     else if(operation == "div") {
+//         return a / b;
+//     }
+// }
+
+// let result = calculator(10, 100, "sub");
+// console.log(result);
+
+
+// function counter(num, max) {
+//     if(num == max) {
+//         return;
+//     }
+//     console.log(num);
+//     counter(num + 1, max)
+// }
+
+// counter(0, 10);
+
+
+
+
+
+// /** Passing arguments to function */
+// let resultOfMultiply = multiply(10,10);
+// console.log(resultOfMultiply);
+// /** Functions in Javascript */
+
+// function person4() {
+//     console.log("person4 function called");
+// }
+
+// function person3() {
+//     console.log("person3 function called");
+//     person4();
+// }
+
+// function person2() {
+//     console.log("person2 function called");
+//     person3();
+
+// }
+
+// function person1() {
+//     console.log("person1 function called");
+//     person2();
+// }
+
+
+// person1();
+// console.log("Hello world");
+
+
 
 /** Defining and calling a function */
+/**
+ * Input -> Function Paramters
+ * Processing / Complex Mathematical Operation / Logic
+ * Ouput ->  
+ */
+
+
+/**
+ * Global Scope
+ */
+// let a =10;
+// let b =100;
+// let c = 1000;
+
+// function calculateSum(a, b, c) {
+//     /** Function scope */
+//     let sum = a + b;
+//     console.log(c);
+//     console.log(sum);
+// }
+
+// /** Function Arguments */
+// let result = calculateSum(200, 100, 23212112); // Function call / Function invocation / Invoked
+// console.log(result);
+
+
+// var x = 100;
+// a();
+// b();
+// console.log(x);
+
+// function a() {
+//     var x = 1;
+//     console.log("value of x is: ", x);
+// }
+
+// function b() {
+//     var x = 1000;
+//     console.log("value of x is: ", x);
+// }
+
+
+// function person4() {
+//     console.log("person4 called");
+// }
+
+// function person3() {
+//     console.log("person3 called");
+//     person4();
+// }
+
+// function person2() {
+//     console.log("person2 called");
+//     person3();
+// }
+
+// function person1() {
+//     console.log("person1 called");
+//     person2();
+//     return;
+// }
+
+
+// person1();
+// console.log("hello world");
+
+// function calculator(a,b, operation) {
+//     if(operation  == "add") {
+//         return a + b;
+//     }
+//     else if(operation  == "sub") {
+//         return a-b;
+//     }
+//     else if(operation == "divide") {
+//         return a /b;
+//     }
+// }
+
+// let result = calculator(100, 1000, "add");
+// console.log(result);
+
+
+/** Recursion */
+function counter(num, max) {
+    console.log(num);
+    if(num == max) {
+        return;
+    }
+    counter(num + 1, max);
+}
+
+counter(0, 1000);
+
 
 
 /** Arguments and return values */
